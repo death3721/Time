630dcd8cd6b849ccc629e96b827f4a075202b680
death3721 <410282238@qq.com>
2024-10-14 21:39:49 +0900

    パラメーター定義

--- Time/ViewController.swift
+++ Time/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var startTimeTextField: UITextField!
+    @IBOutlet weak var endTimeTextField: UITextField!
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var checkButton: UIButton!
+    @IBOutlet weak var table: UITableView!
+    
+    var dataArray = Array<Any>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.table.delegate = self
+        self.table.dataSource = self
     }
-
-
 }
 
+extension ViewController {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.selectionStyle = .none
+        
+        return cell
+    }
+}
